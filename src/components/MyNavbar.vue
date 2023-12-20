010fc7d07c9e748321ac1936e12dce5cb4e02ae0
Que Mnqika <qaqambilemnqika23@gmail.com>
2023-12-20 15:04:37 +0200

    first commit of the project

---
+++ src/components/MyNavbar.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>NAVBAR</h1>
+</template>
+
+<script>
+    export default{
+        data (){
+            return{
+
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ src/views/MyTestimonial.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>Testimonial</h1>
+</template>
+
+<script>
+    export default{
+        data (){
+            return{
+
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
